854146ce68c73ad25e0ebd9e74609b2ba8e328ef
SandunSenevirathna <sandunsandatharu@gmail.com>
2023-11-17 18:23:29 +0530

    add insertUserData and getUserData to backend

--- client/src/Modules/MainForm.jsx
+++ client/src/Modules/MainForm.jsx
@@ -1,38 +1,100 @@
 import React, { useState } from 'react';
 import { Avatar, Box, Button, Paper, TextField } from '@mui/material';
-
 import Person2Icon from '@mui/icons-material/Person2';
-const MainForm = () => {
-    const [showPassword, setShowPassword] = React.useState(false);
+import axios from 'axios';
 
-    const handleClickShowPassword = () => setShowPassword((show) => !show);
+const MainForm = () => {
+    const [isAvailable, setIsAvailable] = useState(false);
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleInsert = async () => {
+        try {
+            // Send a POST request to the backend endpoint with the user data
+            const response = await axios.post('http://localhost:8085/insertUserData', {
+                username,
+                password,
+            });
+
+            console.log(response.data);
+
+            const userDataResponse = await axios.get('http://localhost:8085/getUserData');
+            console.log('Updated UserData:', userDataResponse.data);
+
+        } catch (error) {
+            console.error('Error while inserting user data:', error);
+        }
+    };
+
+    const handleUpdate = () => {
+        // Implement logic for update operation
+        console.log('Update button clicked');
+    };
+
+    const handleDelete = () => {
+        // Implement logic for delete operation
+        console.log('Delete button clicked');
+    };
 
     return (
         <Box
             display="flex"
             justifyContent="center"
             alignItems="center"
-            height="100vh"  // This ensures the container takes the full height of the viewport
+            height="100vh"
         >
-            <Paper elevation={3} style={{ width: '300px', minHeight: '200px', height: 'auto', borderRadius: '15px' }}>
+            <Paper elevation={3} style={{ minWidth: '300px', width: 'auto', minHeight: '200px', height: 'auto', borderRadius: '15px' }}>
                 <Box
                     display="flex"
                     flexDirection={'column'}
                     justifyContent="center"
-                    alignItems="center">
+                    alignItems="center"
+                >
                     <Box mt={2}>
                         <Avatar sx={{ width: 56, height: 56 }}>
                             <Person2Icon sx={{ fontSize: 40 }} />
                         </Avatar>
                     </Box>
                     <Box mt={2}>
-                        <TextField id="outlined-basic" label="Username" variant="outlined" />
+                        <TextField
+                            id="outlined-basic"
+                            label="Username"
+                            variant="outlined"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                        />
                     </Box>
-                    <Box mt={1} >
-                        <TextField id="outlined-basic" label="Password" variant="outlined" type="password" />
+                    <Box mt={1}>
+                        <TextField
+                            id="outlined-basic"
+                            label="Password"
+                            variant="outlined"
+                            type="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
                     </Box>
-                    <Box mt={2} mb={3}>
-                        <Button variant="contained">Insert</Button>
+                    <Box display={'flex'} flexDirection={'row'} mt={2} mb={3}>
+                        {isAvailable ? (
+                            <>
+                                <Box ml={0.5} mr={0.5}>
+                                    <Button variant="contained" color="warning" onClick={handleUpdate}>
+                                        Update
+                                    </Button>
+                                </Box>
+                                <Box ml={0.5} mr={0.5}>
+                                    <Button variant="contained" color="error" onClick={handleDelete}>
+                                        Delete
+                                    </Button>
+                                </Box>
+                            </>
+                        ) : (
+                            <Box ml={0.5} mr={0.5}>
+                                <Button variant="contained" color="success" onClick={handleInsert}>
+                                    Insert
+                                </Button>
+                            </Box>
+                        )}
                     </Box>
                 </Box>
             </Paper>
--- server/app.js
+++ server/app.js
@@ -5,11 +5,35 @@ const cors = require('cors');
 const app = express();
 const port = 8085;
 
+// Assuming UserData is an array for storing multiple user data
+const UserData = [];
+
 app.use(bodyParser.json());
 app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Hello from Backend!');
+});
+
+// Endpoint to handle user data from the frontend
+app.post('/insertUserData', (req, res) => {
+    const { username, password } = req.body;
+
+    // Check if both username and password are provided
+    if (username && password) {
+        // Create a new user object and add it to UserData array
+        const newUser = { username, password };
+        UserData.push(newUser);
+
+        console.log('User data saved:', newUser);
+        res.status(200).json({ message: 'User data saved successfully' });
+    } else {
+        res.status(400).json({ message: 'Invalid data. Both username and password are required.' });
+    }
+});
+
+app.get('/getUserData', (req, res) => {
+    res.status(200).json(UserData);
 });
 
 app.listen(port, () => {
